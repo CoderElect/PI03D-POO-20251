15fb28cc382d6ca4dde69d5ccc46623cc7eabf1b
Miguel Angel Giraldo Mancilla <miguela.giraldom@uqvirtual.edu.co>
2025-02-27 11:01:26 -0500

    update

--- calculadora/src/main/java/co/edu/uniquindio/Calculadora.java
+++ calculadora/src/main/java/co/edu/uniquindio/Calculadora.java
@@ -1,32 +1,32 @@
-package co.edu.uniquindio;
-
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Calculadora {
 
-    public static int sumar(int valor1, int valor2){
-        int resultado = valor1 + valor2;
-        return resultado;
+    public static int sumar(int a, int b) {
+        return a + b;
     }
 
-    public static int restar(int valor1, int valor2){
-        int resultado = valor1 - valor2;
-        return resultado;
+    public static int restar(int a, int b) {
+        return a - b;
     }
 
-    public static int multiplicar(int valor1, int valor2){
-        int resultado = valor1 * valor2;
-        return resultado;
+    public static int multiplicar(int a, int b) {
+        return a * b;
     }
 
+<<<<<<< Updated upstream
     public static int dividir(int valor1, int valor2){
         int resultado = 0;
         if(valor2 != 0){
             resultado = valor1 / valor2;
         }
         return resultado;
+=======
+    public static double dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: No se puede dividir por cero.");
+            return 0;
+        }
+        return (double) a / b;
+>>>>>>> Stashed changes
     }
 
     public static String calculadora(int operador, int valor1, int valor2){
@@ -58,6 +58,7 @@ public class Calculadora {
     }
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 
         Scanner scanner = new Scanner(System.in);
 
@@ -71,5 +72,21 @@ public class Calculadora {
         int valor2 = scanner.nextInt();
 
         System.out.println(calculadora2(operador, valor1, valor2));
+=======
+        System.out.println("El valor de operación suma es: " + sumar(10, 2));
+        System.out.println("El valor de operación resta es: " + restar(10, 2));
+        System.out.println("El valor de operación multiplicación es: " + multiplicar(10, 2));
+        System.out.println("El valor de operación división es: " + dividir(10, 2));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el valor 1: ");
+        int valor1 = scanner.nextInt();
+        System.out.print("Ingrese el valor 2: ");
+        int valor2 = scanner.nextInt();
+
+        System.out.println("El valor de operación suma es: " + sumar(valor1, valor2));
+        scanner.close();
+>>>>>>> Stashed changes
     }
 }
+
